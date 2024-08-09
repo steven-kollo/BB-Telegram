28970ba92167de413e63d9017b556ed134bc4f7b
Steven <steven.kollo.workspace@gmail.com>
2024-08-09 14:32:03 +0400

    naming fixes

--- flask/static/form-builder.js
+++ flask/static/form-builder.js
@@ -3,7 +3,6 @@ const json_g = fetch("/static/groups.json")
     .then(response => response.json()) 
     .then(json => add_services(json.groups))
 
-
 function add_services(groups) {
     groups = add_ids(groups)
     const parent = document.getElementById("group-container")
@@ -20,8 +19,8 @@ function add_services(groups) {
         )
         const subgroup_container = document.getElementById(`g${group.id}s`)
         for (const item of group.items) {
-            const element = build_HTML_item(item)
-            subgroup_container.appendChild(element)
+            const service = build_HTML_service(item)
+            subgroup_container.appendChild(service)
         }
     }
     $('.service-checkbox').click(function(e) {
@@ -61,17 +60,17 @@ function build_HTML_group_collapse(group) {
     return group_collapse
 }
 
-function build_HTML_item(item) {
-    const element = document.createElement('div')
-    element.innerHTML = `
-    <div class="form-check">
-        <input data-toggle="collapse" role="button" aria-expanded="false" class="form-check-input service-checkbox" type="checkbox" href="#g${item.g}s${item.s}c" aria-controls="g${item.g}s${item.s}c" id="g${item.g}s${item.s}" name="g${item.g}s${item.s}">
-        <label class="form-check-label" for="g${item.g}s${item.s}">${item.name}<span class="badge badge-primary">${item.price}</span></label>
-    </div>
-    <div class="collapse ml-3" id="g${item.g}s${item.s}c">
-        <textarea placeholder="${item.tip}" class="form-control" id="g${item.g}s${item.s}t" rows="3" name="g${item.g}s${item.s}t"></textarea>
-    </div>
+function build_HTML_service(item) {
+    const service = document.createElement('div')
+    service.innerHTML = `
+        <div class="form-check">
+            <input data-toggle="collapse" role="button" aria-expanded="false" class="form-check-input service-checkbox" type="checkbox" href="#g${item.g}s${item.s}c" aria-controls="g${item.g}s${item.s}c" id="g${item.g}s${item.s}" name="g${item.g}s${item.s}">
+            <label class="form-check-label" for="g${item.g}s${item.s}">${item.name}<span class="badge badge-primary">${item.price}</span></label>
+        </div>
+        <div class="collapse ml-3" id="g${item.g}s${item.s}c">
+            <textarea placeholder="${item.tip}" class="form-control" id="g${item.g}s${item.s}t" rows="3" name="g${item.g}s${item.s}t"></textarea>
+        </div>
     `
-    element.setAttribute("id", `g${item.g}s${item.s}e`)
-    return element
+    service.setAttribute("id", `g${item.g}s${item.s}e`)
+    return service
 }
